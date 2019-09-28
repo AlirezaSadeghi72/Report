69ba9b6dd8aa599ad54efd784a1cc486bd80c7fa
Alireza Sadeghi <dev.alirezasadeghi@gmail.com>
2019-09-28 17:21:53 +0330

    add handler to ReportSarfasl

--- ReportSarfasl/ReportSarfasl.cs
+++ ReportSarfasl/ReportSarfasl.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.Design;
 using System.Drawing;
+using System.Reflection;
 using System.Windows.Forms;
 
 namespace ReportSarfasl
@@ -29,194 +31,182 @@ namespace ReportSarfasl
 
         private void InitializeComponent()
         {
-            pnlHeader = new Panel();
-            pnlMain = new Panel();
-            pnlFooter = new Panel();
-            txtFilter = new TextBox();
-            dgvSarfasl = new DataGridView();
-            txtSarfasl = new TextBox();
-            txtZirSarfasl = new TextBox();
-            lblZirSarfasl = new Label();
-            btnShow = new Button();
-            btnPrint = new Button();
-            row = new DataGridViewTextBoxColumn();
-            lblSarfasls = new Label();
-            pnlHeader.SuspendLayout();
-            pnlMain.SuspendLayout();
-            pnlFooter.SuspendLayout();
-            ((ISupportInitialize) dgvSarfasl).BeginInit();
-            SuspendLayout();
+            this.pnlHeader = new System.Windows.Forms.Panel();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.txtZirSarfasl = new System.Windows.Forms.TextBox();
+            this.lblZirSarfasl = new System.Windows.Forms.Label();
+            this.txtSarfasl = new System.Windows.Forms.TextBox();
+            this.lblSarfasls = new System.Windows.Forms.Label();
+            this.pnlMain = new System.Windows.Forms.Panel();
+            this.dgvSarfasl = new System.Windows.Forms.DataGridView();
+            this.row = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtFilter = new System.Windows.Forms.TextBox();
+            this.pnlFooter = new System.Windows.Forms.Panel();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.pnlHeader.SuspendLayout();
+            this.pnlMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSarfasl)).BeginInit();
+            this.pnlFooter.SuspendLayout();
+            this.SuspendLayout();
             // 
             // pnlHeader
             // 
-            pnlHeader.Controls.Add(btnShow);
-            pnlHeader.Controls.Add(txtZirSarfasl);
-            pnlHeader.Controls.Add(lblZirSarfasl);
-            pnlHeader.Controls.Add(txtSarfasl);
-            pnlHeader.Controls.Add(lblSarfasls);
-            pnlHeader.Dock = DockStyle.Top;
-            pnlHeader.Location = new Point(0, 0);
-            pnlHeader.Name = "pnlHeader";
-            pnlHeader.Size = new Size(1200, 83);
-            pnlHeader.TabIndex = 0;
-            // 
-            // pnlMain
-            // 
-            pnlMain.Controls.Add(dgvSarfasl);
-            pnlMain.Controls.Add(txtFilter);
-            pnlMain.Dock = DockStyle.Fill;
-            pnlMain.Location = new Point(0, 83);
-            pnlMain.Name = "pnlMain";
-            pnlMain.Size = new Size(1200, 444);
-            pnlMain.TabIndex = 1;
-            // 
-            // pnlFooter
-            // 
-            pnlFooter.Controls.Add(btnPrint);
-            pnlFooter.Dock = DockStyle.Bottom;
-            pnlFooter.Location = new Point(0, 485);
-            pnlFooter.Name = "pnlFooter";
-            pnlFooter.Size = new Size(1200, 42);
-            pnlFooter.TabIndex = 2;
-            // 
-            // txtFilter
-            // 
-            txtFilter.Dock = DockStyle.Top;
-            txtFilter.Location = new Point(0, 0);
-            txtFilter.Name = "txtFilter";
-            txtFilter.Size = new Size(1200, 20);
-            txtFilter.TabIndex = 0;
-            // 
-            // dgvSarfasl
-            // 
-            dgvSarfasl.AllowUserToAddRows = false;
-            dgvSarfasl.AllowUserToDeleteRows = false;
-            dgvSarfasl.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvSarfasl.Columns.AddRange(row);
-            dgvSarfasl.Dock = DockStyle.Fill;
-            dgvSarfasl.Location = new Point(0, 20);
-            dgvSarfasl.Name = "dgvSarfasl";
-            dgvSarfasl.ReadOnly = true;
-            dgvSarfasl.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-            dgvSarfasl.Size = new Size(1200, 424);
-            dgvSarfasl.TabIndex = 1;
-            dgvSarfasl.CellDoubleClick += dgvSarfasl_CellDoubleClick;
-            dgvSarfasl.KeyDown += dgvSarfasl_KeyDown;
-            // 
-            // txtSarfasl
-            // 
-            txtSarfasl.Location = new Point(691, 36);
-            txtSarfasl.Name = "txtSarfasl";
-            txtSarfasl.ReadOnly = true;
-            txtSarfasl.Size = new Size(368, 20);
-            txtSarfasl.TabIndex = 1;
-            txtSarfasl.TextAlign = HorizontalAlignment.Center;
-            txtSarfasl.KeyDown += this.txtSarfasl_KeyDown;
-            // 
-            // txtZirSarfasl
-            // 
-            txtZirSarfasl.Location = new Point(221, 36);
-            txtZirSarfasl.Name = "txtZirSarfasl";
-            txtZirSarfasl.ReadOnly = true;
-            txtZirSarfasl.Size = new Size(368, 20);
-            txtZirSarfasl.TabIndex = 3;
-            txtZirSarfasl.TextAlign = HorizontalAlignment.Center;
-            txtZirSarfasl.KeyDown += this.txtZirSarfasl_KeyDown;
-            // 
-            // lblZirSarfasl
-            // 
-            lblZirSarfasl.AutoSize = true;
-            lblZirSarfasl.Location = new Point(595, 39);
-            lblZirSarfasl.Name = "lblZirSarfasl";
-            lblZirSarfasl.Size = new Size(80, 13);
-            lblZirSarfasl.TabIndex = 2;
-            lblZirSarfasl.Text = "زیر سرفصل ها :";
+            this.pnlHeader.Controls.Add(this.btnShow);
+            this.pnlHeader.Controls.Add(this.txtZirSarfasl);
+            this.pnlHeader.Controls.Add(this.lblZirSarfasl);
+            this.pnlHeader.Controls.Add(this.txtSarfasl);
+            this.pnlHeader.Controls.Add(this.lblSarfasls);
+            this.pnlHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlHeader.Location = new System.Drawing.Point(0, 0);
+            this.pnlHeader.Name = "pnlHeader";
+            this.pnlHeader.Size = new System.Drawing.Size(1200, 83);
+            this.pnlHeader.TabIndex = 0;
             // 
             // btnShow
             // 
-            btnShow.Location = new Point(86, 34);
-            btnShow.Name = "btnShow";
-            btnShow.Size = new Size(75, 23);
-            btnShow.TabIndex = 4;
-            btnShow.Text = "نمایش";
-            btnShow.UseVisualStyleBackColor = true;
-            btnShow.Click += btnShow_Click;
+            this.btnShow.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnShow.Location = new System.Drawing.Point(86, 34);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(75, 23);
+            this.btnShow.TabIndex = 4;
+            this.btnShow.Text = "نمایش";
+            this.btnShow.UseVisualStyleBackColor = true;
             // 
-            // btnPrint
+            // txtZirSarfasl
             // 
-            btnPrint.Location = new Point(42, 9);
-            btnPrint.Name = "btnPrint";
-            btnPrint.Size = new Size(75, 23);
-            btnPrint.TabIndex = 0;
-            btnPrint.Text = "چاپ";
-            btnPrint.UseVisualStyleBackColor = true;
+            this.txtZirSarfasl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtZirSarfasl.Location = new System.Drawing.Point(221, 36);
+            this.txtZirSarfasl.Name = "txtZirSarfasl";
+            this.txtZirSarfasl.ReadOnly = true;
+            this.txtZirSarfasl.Size = new System.Drawing.Size(368, 20);
+            this.txtZirSarfasl.TabIndex = 3;
+            this.txtZirSarfasl.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
-            // row
+            // lblZirSarfasl
             // 
-            row.HeaderText = "ردیف";
-            row.Name = "row";
-            row.ReadOnly = true;
-            row.Visible = false;
+            this.lblZirSarfasl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblZirSarfasl.AutoSize = true;
+            this.lblZirSarfasl.Location = new System.Drawing.Point(595, 39);
+            this.lblZirSarfasl.Name = "lblZirSarfasl";
+            this.lblZirSarfasl.Size = new System.Drawing.Size(80, 13);
+            this.lblZirSarfasl.TabIndex = 2;
+            this.lblZirSarfasl.Text = "زیر سرفصل ها :";
+            // 
+            // txtSarfasl
+            // 
+            this.txtSarfasl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSarfasl.Location = new System.Drawing.Point(691, 36);
+            this.txtSarfasl.Name = "txtSarfasl";
+            this.txtSarfasl.ReadOnly = true;
+            this.txtSarfasl.Size = new System.Drawing.Size(368, 20);
+            this.txtSarfasl.TabIndex = 1;
+            this.txtSarfasl.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // lblSarfasls
             // 
-            lblSarfasls.AutoSize = true;
-            lblSarfasls.Location = new Point(1065, 39);
-            lblSarfasls.Name = "lblSarfasls";
-            lblSarfasls.Size = new Size(63, 13);
-            lblSarfasls.TabIndex = 0;
-            lblSarfasls.Text = "سرفصل ها :";
+            this.lblSarfasls.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblSarfasls.AutoSize = true;
+            this.lblSarfasls.Location = new System.Drawing.Point(1065, 39);
+            this.lblSarfasls.Name = "lblSarfasls";
+            this.lblSarfasls.Size = new System.Drawing.Size(63, 13);
+            this.lblSarfasls.TabIndex = 0;
+            this.lblSarfasls.Text = "سرفصل ها :";
+            // 
+            // pnlMain
+            // 
+            this.pnlMain.Controls.Add(this.dgvSarfasl);
+            this.pnlMain.Controls.Add(this.txtFilter);
+            this.pnlMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlMain.Location = new System.Drawing.Point(0, 83);
+            this.pnlMain.Name = "pnlMain";
+            this.pnlMain.Size = new System.Drawing.Size(1200, 444);
+            this.pnlMain.TabIndex = 1;
+            // 
+            // dgvSarfasl
+            // 
+            this.dgvSarfasl.AllowUserToAddRows = false;
+            this.dgvSarfasl.AllowUserToDeleteRows = false;
+            this.dgvSarfasl.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSarfasl.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.row});
+            this.dgvSarfasl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvSarfasl.Location = new System.Drawing.Point(0, 20);
+            this.dgvSarfasl.Name = "dgvSarfasl";
+            this.dgvSarfasl.ReadOnly = true;
+            this.dgvSarfasl.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvSarfasl.Size = new System.Drawing.Size(1200, 424);
+            this.dgvSarfasl.TabIndex = 1;
+            // 
+            // row
+            // 
+            this.row.HeaderText = "ردیف";
+            this.row.Name = "row";
+            this.row.ReadOnly = true;
+            this.row.Visible = false;
+            // 
+            // txtFilter
+            // 
+            this.txtFilter.Dock = System.Windows.Forms.DockStyle.Top;
+            this.txtFilter.Location = new System.Drawing.Point(0, 0);
+            this.txtFilter.Name = "txtFilter";
+            this.txtFilter.Size = new System.Drawing.Size(1200, 20);
+            this.txtFilter.TabIndex = 0;
+            // 
+            // pnlFooter
+            // 
+            this.pnlFooter.Controls.Add(this.btnPrint);
+            this.pnlFooter.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnlFooter.Location = new System.Drawing.Point(0, 485);
+            this.pnlFooter.Name = "pnlFooter";
+            this.pnlFooter.Size = new System.Drawing.Size(1200, 42);
+            this.pnlFooter.TabIndex = 2;
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Location = new System.Drawing.Point(42, 9);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(75, 23);
+            this.btnPrint.TabIndex = 0;
+            this.btnPrint.Text = "چاپ";
+            this.btnPrint.UseVisualStyleBackColor = true;
             // 
             // ReportSarfasl
             // 
-            Controls.Add(pnlFooter);
-            Controls.Add(pnlMain);
-            Controls.Add(pnlHeader);
-            Name = "ReportSarfasl";
-            RightToLeft = RightToLeft.Yes;
-            Size = new Size(1200, 527);
-            pnlHeader.ResumeLayout(false);
-            pnlHeader.PerformLayout();
-            pnlMain.ResumeLayout(false);
-            pnlMain.PerformLayout();
-            pnlFooter.ResumeLayout(false);
-            ((ISupportInitialize) dgvSarfasl).EndInit();
-            ResumeLayout(false);
+            this.Controls.Add(this.pnlFooter);
+            this.Controls.Add(this.pnlMain);
+            this.Controls.Add(this.pnlHeader);
+            this.Name = "ReportSarfasl";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Size = new System.Drawing.Size(1200, 527);
+            this.pnlHeader.ResumeLayout(false);
+            this.pnlHeader.PerformLayout();
+            this.pnlMain.ResumeLayout(false);
+            this.pnlMain.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSarfasl)).EndInit();
+            this.pnlFooter.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #region Method
 
-        protected virtual void OpenZirSarfasl()
-        {
-            //if (dgvSarfasl.SelectedRows.Count > 0)
-            //{
-            //    //باز کردن زیر سرفصل های سرفصل انتخابی داخل گرید
-            //}
-        }
+
 
         #endregion
 
         #region Event Controls
 
-        //private void txtSarfasl_KeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
-        //{
-        //    if (e.KeyCode == Keys.Enter)
-        //    {
-        //        var sarfasl = new ListSafaslaOrZirSarfasls(1,  _listSar);
-        //        sarfasl.Show();
-        //        _listSar = sarfasl.listSelectes;
-        //    }
-        //}
 
-        //private void txtZirSarfasl_KeyDown(object sender, KeyEventArgs e)
-        //{
-        //    if (e.KeyCode == Keys.Enter)
-        //    {
-        //        var zirsarfasl = new ListSafaslaOrZirSarfasls(2 ,_listZirSar);
-        //        zirsarfasl.Show();
-        //        _listZirSar = zirsarfasl.listSelectes;
-        //    }
-        //}
 
         private void btnShow_Click(object sender, EventArgs e)
         {
@@ -227,15 +217,59 @@ namespace ReportSarfasl
 
         private void dgvSarfasl_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode == Keys.Enter) OpenZirSarfasl();
+            if (e.KeyCode == Keys.Enter) _openZirSarfasl();
         }
 
         private void dgvSarfasl_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
         {
-            OpenZirSarfasl();
+            _openZirSarfasl();
         }
 
         #endregion
+
+        #endregion
+
+        #region Event Handler
+
+        public event EventHandler Sarfasl_KeyDownEnter;
+        private void txtSarfasl_KeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
+        {
+            if (this.Sarfasl_KeyDownEnter != null)
+            {
+                if (e.KeyCode == Keys.Enter)
+                {
+                    this.Sarfasl_KeyDownEnter(this, e);
+                    //var sarfasl = new ListSafaslaOrZirSarfasls(1, _listSar);
+                    //sarfasl.Show();
+                    //_listSar = sarfasl.listSelectes;
+                }
+            }
+        }
+
+        public event EventHandler ZirSarfasl_KeyDownEnter;
+        private void txtZirSarfasl_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (this.ZirSarfasl_KeyDownEnter != null)
+            {
+                if (e.KeyCode == Keys.Enter)
+                {
+                    this.ZirSarfasl_KeyDownEnter(this, e);
+                    //var zirsarfasl = new ListSafaslaOrZirSarfasls(2, _listZirSar);
+                    //zirsarfasl.Show();
+                    //_listZirSar = zirsarfasl.listSelectes;
+                }
+            }
+        }
+
+        public event EventHandler OpenZirSarfasl;
+        protected void _openZirSarfasl(object sender, KeyEventArgs e)
+        {
+            if (dgvSarfasl.SelectedRows.Count > 0)
+            {
+                this.OpenZirSarfasl(this,e);
+                //باز کردن زیر سرفصل های سرفصل انتخابی داخل گرید
+            }
+        }
 
         #endregion
     }
