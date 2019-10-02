6f137ecc53b307522a3bac868b36bbab9bcfc20a
Alireza Sadeghi <dev.alirezasadeghi@gmail.com>
2019-10-02 11:53:36 +0330

    add base form and fix UI

--- ReportSarfasl/Program.cs
+++ ReportSarfasl/Program.cs
@@ -15,7 +15,7 @@ namespace ReportSarfasl
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            Application.Run(new BaseForm());
         }
     }
 }
