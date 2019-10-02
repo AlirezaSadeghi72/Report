e895c9ebe56e182ee4283a8552efc56f483d1414
Alireza Sadeghi <dev.alirezasadeghi@gmail.com>
2019-10-02 15:41:00 +0330

    send metod ShowDialog to class ExtensionMethod

--- ReportSarfasl/ExtensionMethod.cs
+++ ReportSarfasl/ExtensionMethod.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using ReportSarfasl.Forms;
 
 namespace ReportSarfasl
 {
@@ -11,6 +14,29 @@ namespace ReportSarfasl
         public static string ToMan(this decimal i)
         {
             return i.ToString("#,0.0000");
+        }
+
+        public static void ShowDialog(this DefultForm form, UserControl Childe, Size sizeForm)
+        {
+            using (var Temp = new System.Windows.Forms.Form())
+            {
+                //Temp.Dock = System.Windows.Forms.DockStyle.Fill;
+                Temp.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+                Temp.BackColor = System.Drawing.Color.SlateGray;
+                Temp.Opacity = 0.7D;
+                Temp.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+                Temp.ShowInTaskbar = false;
+                Temp.Load += (sender, e) =>
+                {
+                    Childe.Dock = DockStyle.Fill;
+                    form.panel1.Controls.Add(Childe);
+                    form.Size = sizeForm;
+                    form.StartPosition = FormStartPosition.CenterParent;
+                    form.ShowDialog();
+                    Temp.Close();
+                };
+                Temp.ShowDialog();
+            }
         }
     }
 }
